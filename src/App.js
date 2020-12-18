6e275e8faf3f3c455f3461c65d08b67cf43e7075
Bishesh <bishesh.sunam@gmail.com>
2020-12-19 00:39:46 +1300

    user is logged in state using redux

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Sidebar from './Components/Sidebar';
 import Feed from './Components/Feed';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
+import Login from './Components/Login';
+import { auth } from './firebase';
+
+
+
 
 function App() {
+
+  // pulling user from userSlice
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+   auth.onAuthStateChanged(userAuth =>{
+     if(userAuth){
+       //user is logged in
+       dispatch(login({
+         email: userAuth.email,
+         uid: userAuth.uid,
+         displayName: userAuth.displayName,
+         photoUrl: userAuth.photoURL,
+       }))
+     } else{
+       //user is logged out
+       dispatch(logout());
+     }
+   })
+  }, [])
+
   return (
     <div className="app">
       <Header/>
+       {!user ? <Login /> :(
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+          </div>
+       )}
       
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        
-      </div>
     </div>
   );
 }
