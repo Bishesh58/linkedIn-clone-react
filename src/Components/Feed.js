8a84f3daa10b3078c47b044592227574f098c6f1
Bishesh <bishesh.sunam@gmail.com>
2020-12-19 02:10:48 +1300

    news feed information updated

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -9,10 +9,12 @@ import EventNoteIcon from '@material-ui/icons/EventNote';
 import Post from './Post';
 import { db } from '../firebase'; 
 import firebase from 'firebase';
+import { selectUser } from '../features/userSlice';
+import { useSelector } from 'react-redux';
 
 
 function Feed() {
-
+    const user = useSelector(selectUser);
     const [input, setInput] = useState("");
     const [posts, setPosts] = useState([])
     
@@ -31,10 +33,10 @@ function Feed() {
         e.preventDefault();
 
         db.collection('posts').add({
-            name: 'Bishesh sunam',
-            description: 'this is a test',
+            name: user.displayName,
+            description: user.email,
             message: input,
-            photoUrl: "",
+            photoUrl: user.photoUrl || "",
             timestamp:firebase.firestore.FieldValue.serverTimestamp(),
         });
         setInput("");
